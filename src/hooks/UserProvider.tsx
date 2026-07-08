f72f7307ce81a7569ac1b2fa13cf41f3adc4855b
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-07-08 20:45:29 +0200

    feat: finish user management feature

--- src/hooks/UserProvider.tsx
+++ src/hooks/UserProvider.tsx
@@ -1,10 +1,17 @@
 import { userReducer } from "./userReducer";
 import type { ReactNode } from "react";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { UserContext } from "./userContext";
 
 export function UserProvider({ children }: { children: ReactNode }) {
-  const [users, dispatch] = useReducer(userReducer, []);
+  const [users, dispatch] = useReducer(userReducer, [], () => {
+    const storedUsers = localStorage.getItem("users");
+    return storedUsers ? JSON.parse(storedUsers) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
 
   return <UserContext.Provider value={{ users, dispatch }}> {children}</UserContext.Provider>;
 }
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,49 @@
+import { useUserContext } from "../../hooks/userContext";
+import "./user.scss";
+
 function Users() {
-  return <div>Hello from Users</div>;
+  const { users, dispatch } = useUserContext();
+  console.log(users);
+
+  function handleDeleteUser(id: string) {
+    dispatch({ type: "DELETE_USER", payload: id });
+  }
+  return (
+    <div>
+      {users.length === 0 ? (
+        <p>No users found.</p>
+      ) : (
+        <ul className="users">
+          {users.map((user) => (
+            <div className="user-card" key={user.id}>
+              <button onClick={() => handleDeleteUser(user.id)} className="user-card__close">
+                ✕
+              </button>
+              <div className="user-card__left">
+                <img
+                  className="user-card__avatar"
+                  src={user.gender === "male" ? "/images/male.png" : "/images/female.png"}
+                  alt={`${user.username}'s image`}
+                />
+              </div>
+              <div className="user-card__right">
+                <h3 className="user-card__name">{user.username}</h3>
+
+                <div className="user-card__grid">
+                  <div className="user-card__item">📅 {user.birthday}</div>
+                  <div className="user-card__item">📍 {user.postAddress}</div>
+                  <div className="user-card__item">⚧ {user.gender}</div>
+                  <div className="user-card__item">📞 {user.phone}</div>
+                  <div className="user-card__item">✉️ {user.email}</div>
+                  <div className="user-card__item">🌐 {user.website}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Users;
